6965e3fc3e7b6f81126d922417a826e9968993a3
EstexNT <estex64@gmail.com>
2025-12-19 00:10:38 +0300

    Label the input types in commands

--- src/Game/AgbTap/MyFlow.cpp
+++ src/Game/AgbTap/MyFlow.cpp
@@ -14,7 +14,7 @@ bool AgbTap::CMyFlow::_1C(u32 opcode, u32 arg0, const s32 *args) {
     switch (opcode) {
     case TF_AGBTAP_INPUT: {
         CMyChecker *checker = gInputCheckManager->makeNew<CMyChecker>();
-        checker->fn_801E7D5C(4);
+        checker->fn_801E7D5C(eInputType_TriggerA);
         checker->fn_801E7DBC(48.0f, -10.0f, -6.0f, 6.0f, 10.0f);
 
         gInputCheckManager->fn_801E9158(checker, true);
