e596415cac00ede1d0face360ec91cbe337a2479
Angelo <48489278+conhlee@users.noreply.github.com>
2025-12-29 21:05:00 +0100

    GameUtil: Vector header

---
+++ src/GameUtil/Vector.hpp
@@ -0,0 +1,30 @@
+#ifndef GAMEUTIL_VECTOR_HPP
+#define GAMEUTIL_VECTOR_HPP
+
+/*
+    NOTE: this vector class is utilised solely by DebugPrint.
+*/
+
+#include <revolution/types.h>
+
+#include <cstring>
+
+template <typename T>
+class CVector {
+public:
+    virtual ~CVector(void) {
+        delete[] mArray;
+    }
+
+    CVector(void) {
+        mSize = 1;
+        mArray = new T[mSize];
+        memset(mArray, 0x00, mSize * sizeof(T));
+    }
+
+private:
+    s32 mSize;
+    T *mArray;
+};
+
+#endif
