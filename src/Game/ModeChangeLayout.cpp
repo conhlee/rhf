e935d86e24d88a33cf4bbac9972203a5193cfcbb
Angelo <48489278+conhlee@users.noreply.github.com>
2026-01-01 19:44:07 +0100

    ModeChangeLayout: cleanup

--- src/Game/ModeChangeLayout.cpp
+++ src/Game/ModeChangeLayout.cpp
@@ -8,6 +8,11 @@ const char layoutFile_mode_change[] = "mode_change.brlyt";
 
 static const char *layoutFileTable[] = { layoutFile_mode_change, NULL };
 
+enum {
+    mode_change_change_key,
+    mode_change_change_DPD,
+};
+
 void CModeChangeLayout::_10(void) {
     nw4r::lyt::MultiArcResourceAccessor *resAccessor = gLayoutManager->getResAccessor();
 
@@ -19,8 +24,8 @@ void CModeChangeLayout::_10(void) {
     nw4r::lyt::Pane *paneChangeKey = getLayout()->GetRootPane()->FindPaneByName("N_change_key_00");
     nw4r::lyt::Pane *paneChangeDPD = getLayout()->GetRootPane()->FindPaneByName("N_change_DPD_00");
 
-    getAnimation(0)->fn_801D9E70(getLayout(), paneChangeKey, "mode_change_change_key", resAccessor);
-    getAnimation(1)->fn_801D9E70(getLayout(), paneChangeDPD, "mode_change_change_DPD", resAccessor);
+    getAnimation(mode_change_change_key)->fn_801D9E70(getLayout(), paneChangeKey, "mode_change_change_key", resAccessor);
+    getAnimation(mode_change_change_DPD)->fn_801D9E70(getLayout(), paneChangeDPD, "mode_change_change_DPD", resAccessor);
 
     gMessageManager->fn_80088474(getLayout()->GetRootPane());
 
@@ -28,20 +33,20 @@ void CModeChangeLayout::_10(void) {
 }
 
 void CModeChangeLayout::fn_800833E4(void) {
-    getAnimation(0)->playFromBeginForward();
+    getAnimation(mode_change_change_key)->playFromBeginForward();
 
-    if (getAnimation(1)->getIsPlaying()) {
-        getAnimation(1)->playFromBeginReverse();
-        getAnimation(1)->fn_801DA770();
+    if (getAnimation(mode_change_change_DPD)->getIsPlaying()) {
+        getAnimation(mode_change_change_DPD)->playFromBeginReverse();
+        getAnimation(mode_change_change_DPD)->fn_801DA770();
     }
 }
 
 void CModeChangeLayout::fn_8008347C(void) {
-    getAnimation(1)->playFromBeginForward();
+    getAnimation(mode_change_change_DPD)->playFromBeginForward();
 
-    if (getAnimation(0)->getIsPlaying()) {
-        getAnimation(0)->playFromBeginReverse();
-        getAnimation(0)->fn_801DA770();
+    if (getAnimation(mode_change_change_key)->getIsPlaying()) {
+        getAnimation(mode_change_change_key)->playFromBeginReverse();
+        getAnimation(mode_change_change_key)->fn_801DA770();
     }
 }
 
